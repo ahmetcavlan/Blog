fef59e4ae7f5cf9566c80791c37b1f89c766b912
ahmetcavlan <cancavlan@gmail.com>
2015-06-01 00:00:45 +0300

    Added post

---
+++ Blog/Controllers/ImageController.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class FileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /File/
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Images.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.Content_Type);
+        }
+    }
+}
---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+using System.Data.Entity.Infrastructure;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Blog.Controllers
+
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Post/
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var posts = from s in db.Posts
+                        select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                posts = posts.Where(s => s.post_Title.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    posts = posts.OrderByDescending(s => s.post_Title);
+                    break;
+                case "Date":
+                    posts = posts.OrderBy(s => s.post_Date);
+                    break;
+                case "date_desc":
+                    posts = posts.OrderByDescending(s => s.post_Date);
+                    break;
+                default:  // Name ascending 
+                    posts = posts.OrderBy(s => s.post_preview);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(posts.ToPagedList(pageNumber, pageSize));
+        }
+
+
+       
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Post post = db.Posts.Include(s => s.Images).SingleOrDefault(s => s.post_Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "post_Title, post_Content, created_By,post_Date,post_preview")]Post post, HttpPostedFileBase upload)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (upload != null && upload.ContentLength > 0)
+                    {
+                        var image = new Image
+                        {
+                            Image_Name = System.IO.Path.GetFileName(upload.FileName),
+                            Image_Type = ImageType.Avatar,
+                            Content_Type = upload.ContentType
+                        };
+                        using (var reader = new System.IO.BinaryReader(upload.InputStream))
+                        {
+                            image.Content = reader.ReadBytes(upload.ContentLength);
+                        }
+                        post.Images = new List<Image> { image };
+                    }
+                    post.post_Date = DateTime.Now;
+                    post.created_By = User.Identity.Name;
+                    db.Posts.Add(post);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(post);
+        }
+
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Include(s => s.Images).SingleOrDefault(s => s.post_Id == id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id, HttpPostedFileBase upload)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var postToUpdate = db.Posts.Find(id);
+            if (TryUpdateModel(postToUpdate, "",
+               new string[] { "post_Title", "post_Content", "post_preview" }))
+            {
+                try
+                {
+                    if (upload != null && upload.ContentLength > 0)
+                    {
+                        if (postToUpdate.Images.Any(f => f.Image_Type == ImageType.Avatar))
+                        {
+                            db.Images.Remove(postToUpdate.Images.First(f => f.Image_Type == ImageType.Avatar));
+                        }
+                        var image = new Image
+                        {
+                            Image_Name = System.IO.Path.GetFileName(upload.FileName),
+                            Image_Type = ImageType.Avatar,
+                            Content_Type = upload.ContentType
+                        };
+                        using (var reader = new System.IO.BinaryReader(upload.InputStream))
+                        {
+                            image.Content = reader.ReadBytes(upload.ContentLength);
+                        }
+                        postToUpdate.Images = new List<Image> { image };
+                    }
+                    postToUpdate.post_Date = DateTime.Now;
+                    postToUpdate.created_By = User.Identity.Name;
+                    db.Entry(postToUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(postToUpdate);
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Post post = db.Posts.Find(id);
+                db.Posts.Remove(post);
+                db.SaveChanges();
+            }
+            catch (RetryLimitExceededException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
