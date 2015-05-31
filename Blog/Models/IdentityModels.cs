851a9695ef60d03831d6cdd210cf4961a0281d65
ahmetcavlan <cancavlan@gmail.com>
2015-05-31 20:25:54 +0300

    First Commit

---
+++ Blog/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+
+namespace Blog.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string adminName { get; set; }
+        public string adminSurname { get; set; }
+        public string adminEmail { get; set; }
+        public DateTime adminDate { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImagePath> ImagePaths { get; set; }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int post_Id { get; set; }  
+        public string post_Content { get; set; }
+        public string post_Title { get; set; }
+        public string created_By { get; set; }
+        public DateTime post_Date { get; set; }
+        public string post_preview { get; set; }
+        public virtual ICollection<Image> Images { get; set; }
+        public virtual ICollection<ImagePath> ImagePaths { get; set; }
+
+    }
+    public class Image
+    {
+        [Key]
+        public int Image_Id { get; set; }
+        [StringLength(255)]
+        public string Image_Name { get; set; }
+        [StringLength(100)]
+        public string Content_Type { get; set; }
+        public byte[] Content { get; set; }
+        public ImageType Image_Type { get; set; }
+        public int post_Id { get; set; }
+        public virtual Post post { get; set; }
+    }
+    public class ImagePath
+    {
+        [Key]
+        public int ImagePath_Id { get; set; }
+        [StringLength(255)]
+        public string Image_Name { get; set; }
+        public ImageType Image_Type { get; set; }
+        public int post_Id { get; set; }
+        public virtual Post post { get; set; }
+    }
+    public enum ImageType
+    {
+        Avatar = 1, Photo
+    }
+}
